55a71908896d684d9ff0a86bc49d35ceb64a8234
brunning <benjamin.running@gmail.com>
2015-04-09 11:34:29 -0400

    fronted content for article template
    modded index.html (needs to be integrated), added images, css, js

---
+++ js/static-menu.js
@@ -0,0 +1,9 @@
+var num = 0;
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.menu').addClass('fixed');
+    } else {
+        $('.menu').removeClass('fixed');
+    }
+});
