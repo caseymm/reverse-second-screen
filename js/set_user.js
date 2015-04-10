87fd12f1cd5685ea5aa10a99ecf0f68246c98b1b
caseymm <casey.miller@voxmedia.com>
2015-04-10 18:22:51 -0400

    fix some issues with adding users. Still temp fix.

--- js/set_user.js
+++ js/set_user.js
@@ -1,20 +1,14 @@
 (function() {
 
-    // user_data.on('value', function (snapshot) {
-    //   current_id = snapshot.val();
-    // });
-    //
-    //
-
     function checkUser(entered_name){
       var get_users = new Firebase('https://sweltering-torch-4591.firebaseIO.com/users/');
-      get_users.on('value', function (snapshot) {
+      get_users.once('value', function (snapshot) {
         var users = snapshot.val();
         if(users[entered_name]){
-          console.log('great');
         } else {
-          // var get_user = new Firebase('https://sweltering-torch-4591.firebaseIO.com/users/'+entered_name);
-          // var new_user = { entered_name: { "burning_man" : { "slug" : "burning_man1", "volume" : 0.5 }, "space" : { "slug" : "space2", "volume" : 0.5} } }
+          get_users.on('child_changed', function (snapshot) {
+            users = snapshot.val();
+          })
           users[entered_name] = { "burning_man" : { "slug" : "burning_man1", "volume" : 0.5 }, "space" : { "slug" : "space2", "volume" : 0.5} }
           get_users.set(users);
         }
@@ -24,6 +18,7 @@
     $('#nameButton').on('click', function(){
       var entered_name = $('#nameInput').val();
       checkUser(entered_name);
+      $('#nameInput').val('');
     })
 
   $(document).ready(function() {
